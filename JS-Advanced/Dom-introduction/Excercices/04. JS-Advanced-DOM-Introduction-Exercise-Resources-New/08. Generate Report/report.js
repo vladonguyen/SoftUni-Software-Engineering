20a398c4fb6d6e4d86d5decdf1efc51411277fef
vladonguyen <vladonguyen@gmail.com>
2022-02-01 02:09:53 +0200

    1 task

--- JS-Advanced/Dom-introduction/Excercices/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ JS-Advanced/Dom-introduction/Excercices/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,34 @@
 function generateReport() {
-    //TODO
+    let inputs = document.getElementsByTagName('input'); // get the column headers
+    let allTr = document.querySelectorAll('tbody tr'); // get all tr-s with employers data
+
+    let i = 0;
+    let checkedNumbers = []; // put the index of all checked headers in array
+    let categoryNames = []; //take all category(header) names
+    for (let input of inputs) {
+        if (input.checked === true) {
+            checkedNumbers.push(i);
+        };
+        categoryNames.push(input.name)
+        i++;
+    }
+    let storeValidData = []; // put all employers' data in object and then push in array
+    //iterate through all tr with employer data and take checked data
+    for (let tr of allTr) {
+        let allTd = tr.querySelectorAll('td');
+        let i = 0;
+        let obj = {};
+        for (let td of allTd) {
+            if (checkedNumbers.includes(i)) {
+                obj[categoryNames[i]] = td.textContent;
+
+            }
+            i++;
+        }
+        storeValidData.push(obj);
+
+    }
+    const myJSON = JSON.stringify(storeValidData);
+    let output = document.getElementById('output');
+    output.textContent = myJSON;
 }
